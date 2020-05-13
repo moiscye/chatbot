58e05ad0a321f0f5be55ed1e93ab546183ae403b
Moises Cruz <moiscye@gmail.com>
2020-05-14 06:46:23 +1000

    Add shop components

---
+++ client/src/components/shop/ItemList.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { Container } from "semantic-ui-react";
+
+const ItemList = () => <Container>Items</Container>;
+
+export default ItemList;
---
+++ client/src/components/shop/Shop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ItemList from "./ItemList";
+import Layout from "../layout/Layout";
+
+const Shop = () => (
+  <Layout title="Shop" description="Manage your shopping here">
+    <ItemList />
+  </Layout>
+);
+
+export default Shop;
