8838eb4ea234764f16f3d2e576b6795bf03f2917
Moises Cruz <moiscye@gmail.com>
2020-06-09 18:29:20 +1000

    Update on the fulfillment file

--- routes/fulfillment.js
+++ routes/fulfillment.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { fulfillment } = require("../controllers/fulfullment");
+const { fulfillment } = require("../controllers/fulfillment");
 
 router.post("/fulfillment", fulfillment);
 
